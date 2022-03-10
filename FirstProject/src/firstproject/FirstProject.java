68a533f81bd3ab66e5d45768b54ac0b8f02e20c2
Pavel Vácha <pavel.vacha@tul.cz>
2022-03-10 08:26:57 +0100

    Pomoc

--- FirstProject/src/firstproject/FirstProject.java
+++ FirstProject/src/firstproject/FirstProject.java
@@ -14,7 +14,7 @@ public class FirstProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world");
     }
     
 }
